6859832457b718b5ef70b418ce2768ece0fbcd0b
R. A. Souza <alves.wm@gmail.com>
2025-04-26 16:44:43 +0200

    style: lint using biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom'
 
-import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
+import AboutPage from '@/pages/about'
+import BlogPage from '@/pages/blog'
+import DocsPage from '@/pages/docs'
+import IndexPage from '@/pages/index'
+import PricingPage from '@/pages/pricing'
 
 function App() {
-  return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route element={<IndexPage />} path="/" />
+			<Route element={<DocsPage />} path="/docs" />
+			<Route element={<PricingPage />} path="/pricing" />
+			<Route element={<BlogPage />} path="/blog" />
+			<Route element={<AboutPage />} path="/about" />
+		</Routes>
+	)
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-import App from "./App.tsx";
-import { Provider } from "./provider.tsx";
-import "@/styles/globals.css";
+import App from './App.tsx'
+import { Provider } from './provider.tsx'
+import '@/styles/globals.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+// biome-ignore lint/style/noNonNullAssertion: React boilerplate
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Provider>
+				<App />
+			</Provider>
+		</BrowserRouter>
+	</React.StrictMode>,
+)
