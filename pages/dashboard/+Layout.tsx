21888f65fb90ffaf26d9497176747cac4ca45c80
Rodrigo Souza <rodrigo.souza@klarna.com>
2025-04-28 19:37:18 +0200

    implement App Shell layout

---
+++ pages/dashboard/+Layout.tsx
@@ -0,0 +1,24 @@
+import { Breadcrumbs } from "@/components/Breadcrumbs";
+import "../../assets/style.css";
+
+import "../../assets/tailwind.css";
+
+import { AppSidebar } from "@/components/app-sidebar";
+
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+
+export default function LayoutDefault({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <Breadcrumbs />
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
---
+++ pages/dashboard/+config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "vike/types";
+import vikeReact from "vike-react/config";
+
+export const config = {
+  title: "Home",
+  description: "Home page",
+  extends: vikeReact,
+} satisfies Config;
