ccd4d45677223c6c8248d45c7c238bb099c58523
Rodrigo Souza <rodrigo.souza@klarna.com>
2025-04-30 23:31:34 +0200

    add api contracts and ts-rest integration to users controller

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -1,11 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
-
+import { userContract } from '@repo/api-contracts';
+import { tsRestHandler, TsRestHandler } from '@ts-rest/nest';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
   @Get()
+  @TsRestHandler(userContract.getUsers)
   findAll() {
-    return this.usersService.findAll();
+    return tsRestHandler(userContract.getUsers, async () => {
+      const users = await this.usersService.findAll();
+      const transformedUsers = users.map((user) => ({
+        ...user,
+        avatar: user.avatar ?? undefined,
+      }));
+      return {
+        status: 200,
+        body: transformedUsers,
+      };
+    });
   }
 }
