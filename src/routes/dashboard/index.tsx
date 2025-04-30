78e6e579004db28f1afbe3e0092a3ae6d79ed4a4
Rodrigo Souza <rodrigo.souza@klarna.com>
2025-04-30 09:55:59 +0200

    fix layout

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard/")({
@@ -6,13 +7,13 @@ export const Route = createFileRoute("/dashboard/")({
 
 function RouteComponent() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+    <Fragment>
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
       </div>
       <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-    </div>
+    </Fragment>
   );
 }
--- src/routes/dashboard/route.tsx
+++ src/routes/dashboard/route.tsx
@@ -17,7 +17,9 @@ function AppLayoutComponent() {
       <AppSidebar routes={routes} />
       <SidebarInset>
         <Breadcrumbs />
-        <Outlet />
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <Outlet />
+        </div>
       </SidebarInset>
     </SidebarProvider>
   );
