e6f3b17393b59d028b0d8f6094b867f7eda17fd6
Rodrigo Souza <rodrigo.souza@klarna.com>
2025-04-30 21:29:32 +0200

    setup zod and error handler

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './config/validate';
 import { DatabaseModule } from './database/database.module';
+import { APP_PIPE, APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core';
+import { ZodValidationPipe, ZodSerializerInterceptor } from 'nestjs-zod';
+import { HttpExceptionFilter } from './filters/http-exception-filter';
 
 @Module({
   imports: [
@@ -15,6 +18,20 @@ import { DatabaseModule } from './database/database.module';
     DatabaseModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ZodSerializerInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
